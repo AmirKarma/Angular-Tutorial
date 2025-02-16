c0b2414518aa07e154851a579c4b0dae60b0d092
AmirKarma <amir.ebrahimi0k@gmail.com>
2025-02-16 18:33:49 +0100

    angular-tutorial

---
+++ angular-tutorial/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TasksComponent } from "./tasks/tasks.component";
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+import { HeaderComponent } from "./header/header.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, TasksComponent, UserComponent, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId!);
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
+}
