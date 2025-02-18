19d885bc3ca5bfbde1b6ce681fc9310471a2a0b0
AmirKarma <amir.ebrahimi0k@gmail.com>
2025-02-18 21:56:52 +0100

    add onCancel Function

--- angular-tutorial/src/app/tasks/new-task/new-task.component.ts
+++ angular-tutorial/src/app/tasks/new-task/new-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-new-task',
@@ -8,5 +8,9 @@ import { Component, Input } from '@angular/core';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
+  @Output() canceld = new EventEmitter<void>();
 
+  onCancel() {
+    this.canceld.emit();
+  }
 }
