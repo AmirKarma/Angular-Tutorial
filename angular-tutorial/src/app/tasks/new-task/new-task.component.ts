082d2dac779524db5d0436f244acd8b376c07d41
AmirKarma <amir.ebrahimi0k@gmail.com>
2025-04-04 13:58:37 +0200

    add 2 ways binding

--- angular-tutorial/src/app/tasks/new-task/new-task.component.ts
+++ angular-tutorial/src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,18 @@
 import { Component, EventEmitter, Input, Output} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
   @Output() canceld = new EventEmitter<void>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
   onCancel() {
     this.canceld.emit();
