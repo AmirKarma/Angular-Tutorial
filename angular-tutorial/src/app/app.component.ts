906e7457e4cf3651ef8adc22b27e23508d9a1804
AmirKarma <amir.ebrahimi0k@gmail.com>
2025-04-11 14:54:53 +0200

    add module and update npm

--- angular-tutorial/src/app/app.component.ts
+++ angular-tutorial/src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { TasksComponent } from "./tasks/tasks.component";
-import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
-import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, TasksComponent, UserComponent, HeaderComponent],
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
